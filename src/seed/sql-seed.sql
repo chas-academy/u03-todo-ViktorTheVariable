4ebbbc62d3cdb7185eebadaa13e7f719f7dec7b0
Viktor Ekstrom <viktor.ekstrom@chasacademy.se>
2025-01-02 01:39:36 +0100

    modifierade videoGames tabell och la till 2 nya tabeller i sql filen

--- src/seed/sql-seed.sql
+++ src/seed/sql-seed.sql
@@ -1,6 +1,20 @@
+CREATE TABLE users (
+    userID INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(40) NOT NULL UNIQUE
+);
+
+CREATE TABLE lists (
+    listID INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    userID INT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users(userID)
+);
+
 CREATE TABLE videoGames (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    gameID INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(80) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    is_completed BOOLEAN DEFAULT FALSE
+    description VARCHAR(255),
+    is_completed BOOLEAN DEFAULT FALSE,
+    listID INT NOT NULL,
+    FOREIGN KEY (listID) REFERENCES lists(listID)
 );
