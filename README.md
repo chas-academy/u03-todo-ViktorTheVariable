931f7ea65c611004a4b87c0da18a6994dbc67e67
Viktor Ekstrom <viktor.ekstrom@chasacademy.se>
2025-01-11 10:07:15 +0100

    la till kommentarer och förändrade koden till PSR-12 formatet

--- README.md
+++ README.md
@@ -25,10 +25,10 @@ Om tid finnes kommer jag först göra designen lite snyggare med bilder, ikoner,
 ## Utförande
 
 ### ER-Diagram
-![erDiagram](https://github.com/user-attachments/assets/d8938140-2e8a-40de-b6a1-278c0a1ccb49)
+![ER-Diagram](https://github.com/user-attachments/assets/ab4bea14-d1bf-41be-a5f0-381ccc59d793)
 [Länk till ER-Diagram](https://www.figma.com/design/oGqJhH8jCxsHc1e2w2Qg6W/U03-ERDiagram-FigmaSkiss?node-id=0-1&p=f&t=LLaNUQibLpP9yzGb-0)
 <br>
-En tabell kommer behövas då enbart användarens input av titel och beskrivning kommer att läggas in i databasen tillsammans med ID och klar/icke-klar kolumnerna. ID kommer vara ett nummer som automatiskt ökar för varje tabellrad och behöver därför vara INT och auto_increment. Jag har satt denna som primary key för att unikt kunna identifiera varje tabellrad. Title ska vara en sträng med ett spelnamn som användaren skriver in, har satt VARCHAR till 80 för att begränsa antalet tecken i kolumnen då flesta speltitlar har 20-40 tecken och NOT NULL för att ett definierat värde ska finnas i kolumnen. Description ska vara en sträng med spelbeskrivning, VARCHAR(255) för att besgränsa antalet tecken och få en kort beskrivning av spelet. is_completed ska vara en klar/icke-klar ruta som representeras genom ett TRUE/FALSE värde och ska därför vara BOOLEAN med ett DEFAULT värde av FALSE där anävndaren själv bockar av spelet som spelat och gör det till TRUE.
+En tabell kommer behövas då enbart användarens input av titel och beskrivning kommer att läggas in i databasen tillsammans med ID och klar/icke-klar kolumnerna. ID kommer vara ett nummer som automatiskt ökar för varje tabellrad och behöver därför vara INT och auto_increment. Jag har satt denna som primary key för att unikt kunna identifiera varje tabellrad. Title ska vara en sträng med ett spelnamn som användaren skriver in, har satt VARCHAR till 80 för att begränsa antalet tecken i kolumnen då flesta speltitlar har 20-40 tecken och NOT NULL för att ett definierat värde ska finnas i kolumnen. Description ska vara en sträng med spelbeskrivning, VARCHAR(255) för att besgränsa antalet tecken och få en kort beskrivning av spelet. is_completed ska vara en klar/icke-klar ruta som representeras genom ett TRUE/FALSE värde och ska därför vara BOOLEAN med ett DEFAULT värde av FALSE där anävndaren själv bockar av spelet som spelat och gör det till TRUE. Uppdatering: KOm på att jag behöver lite fler tabeller annars kommer man behöva sudda uppgifts tabellen varje gång nån går bort från sidan för att det ska fungera. Och 2 eller flera användare kommer inte kunna använda sidan samtidigt då dessa kommer skapa och sedan visa upp sina egna plus andra användarens spel i listan. jag kopplade ihop tabellerna genon att sätta userID som främmande nyckel i lists tabellen och listID som färmmande nyckel i videoGames tabellen. Uppdatering: Insåg att det räckte med 2 tabeller och pga tidsbrist bytte jag till 2 för att slippa extra tid i logiken med 3 tabeller. Är samma users tabell men som länkar 1:N med videoGames tabellen där videogames byter ut foreign key för listID till userID.
 
 ### Figma-Skiss
 ![Skärmbild 2024-12-29 054915](https://github.com/user-attachments/assets/aa178b8e-0741-44a2-85c6-a54b9a79ebf0)
@@ -41,7 +41,37 @@ Filstruktur skapad i VsCode med filerna: Dockerfile, docker-compose.yml, index.p
 En container skapades med databasen U03-todo. En tabell videoGames enligt ER-Diagrmmet skapades också via sql-seed.sql filen när containern skpades.
 
 ### Skapa en databas och koppla den till PDO
-En tabell videoGames enligt ER-Diagrmmet skapades via sql-seed.sql filen när containern skpades. Databasen kopplades sedan ihop med PDO i db.php filen.
+tre tabeller enligt ER-Diagrmmet skapades via sql-seed.sql filen när containern skpades. Databasen kopplades sedan ihop med PDO i db.php filen.
+
+### Skapa PHP-vyer med grundläggande HTML och css
+html strukturen och css styling skapad för att se ut någolunda som figma skissen. Nu kan jag bygga crud functionerna och testa dem på sidan.
+
+### Bygga mina SQL-Frågor med PDO
+
+1. jag började med att skapa en funktion getUserId som förbreder sql-frågorna SELECT och INSERT med prepared/execute statement för att antingen hämta användarID från databasen om den redan finns eller skapa ett nytt användarID om det som användaren ska skriva in inte finns i databasen.
+2. Jag skapade en funktion addGame för att lägga till ett spelet titel och beskrivning (och för tillfället annan text) i databasen via sql-frågan INSERT och prepared/execute statement om båda formulär fälten är ifyllda. som via användarens inmatning i title och description inputfälten tillsammans med användarID:t lägger till inputs från de båda i respektive kolumn i databasen. Logik skapades för att köra funktionen och formulär i index.php modifierades för att passa ihop med logiken och köra funktionen med en knapptryckning.
+3. Jag skapade en funktion getGames för att hämta all data från databasen som tillhör en visst användarId genom sql-frågan SELECT och prepared/execute statement.
+4. Jag skapade en funktion toggleCheckbox för att toggla en checkbox som iffyld ska lägga in true och inte ifylld lägga in false i databasen.
+5. Skapade sedan en funktion updateGame för att uppdatera en spel med ny titel och beskrivning via sql-frågan UPDATE och prepared/execute statement. och logik för att köra den. I index.php lade jag till ett hidden formulär som visas via en knapptryckning. 
+6. Sist la jag till en funktion för att radera ett spel från listan och spara denna radering i databasen. logik skapades för funktionen och ett modifierat formulär i index.php skapades också för att radera spelet via användares input.
+
+### Koppla ihop vyerna med sin backend
+
+1. Jag skapade logiken för funktionen getUserId med hjälp av en nästlad if-sats som säger att när en POST-request har skickats (som sedan kommer kunna användas av andra funktioner) och knappen för formuläret add_game trycks på skickar en POST-request och slutligen om title och description formulären inte är tomma så körs addGame funktionen och lägger till ett spel i databasen och genererar ett meddelande om att det lyckades annars ett meddelande att det inte lyckats. 
+
+2. Logiken till addGame tar användarens inmatning i title och description inputfält tillsammans med användarID:t och lägger till inputs från de båda i respektive kolumn i databasen. Logik skapades för att köra funktionen och formulär i index.php modifierades för att passa ihop med logiken och köra funktionen med en knapptryckning.
+
+3.  Jag skapade logiken för getGames genom en variabel $showlist med värdet false för att i index.php sätta värdet till true via en vilkorsats om knappen för att visa listan trycks på av användaren. När knapptryckning sker repreneteras datan från databasen genom en tabell som skapas med hjälp av en foreach-loop där varje objekt i arrayen innehållande alla rader i databasen för den userID genereras på sidan.
+
+4. Logiken för toggleCheckbox kollar om knappen för checkboxen is_completed skickar en POST-request och kollar om användaren fyllt i checkbox, om ja så sätts is_complete i databasen till true annars false och denna status sparas och med hjälp av logik i formuläret i index.php visar rätt status i checkboxen. $showlist sätts också till true för att visa tabellen med data efter uppdateringen.
+
+5. Logik skapades för updateGame och kollar om knappen för formuläret update_game skickar en POST-request genom att användaren klickar på update knappen som skapats i ett formulär. Om ja så körs funktionen och is_completed uppdateras samt $showlist sätts till true för att visa tabellen med data efter uppdateringen. Formuläret som skapas är ett hidden formulär som visas och göms via en knapptryckning.
+
+6. logik skapades för deleteGame genom att kolla om knappen för formuläret delete_game skickar en POST-request och ett modifierat formulär i index.php skapades också för att radera spelet via användares input.
+
+7. Ja lade till sist in logik som kontrollerar om en användarnamn har skickats i formuläret och hämtar användarens ID och spel från databasen om användarnamnet är giltigt.
+
+
 ---
 
 Viktor Ekström
--- src/index.php
+++ src/index.php
@@ -27,23 +27,28 @@ require_once 'crud-functions.php';
         <main id="main" class="flex">
             <h2>Create a list of games</h2>
             <form method="post" class="flex">
-            <label for="username" class="label">Type in your username:</label>
-            <input type="text" name="username" id="username" class="input" required maxlength="40" value="<?php echo htmlspecialchars($currentUsername); ?>">
-            <label for="title" class="label">Type in the title of the game:</label>
-            <input type="text" name="title" id="title" class="input" maxlength="80">
-            <label for="description" class="label">Type in a short description of the game:</label>
-            <textarea rows="4" cols="30" name="description" id="description" class="description" maxlength="255"></textarea>
-            <input type="submit" name="add_game" value="Add a game to a list" class="input">
-            <?php if ($message): ?>
-                <p class="input"><?php echo htmlspecialchars($message); ?></p>
-            <?php endif; ?>
-            <input type='hidden' name='show_list' value='1'>
-            <input type='submit' value='Show your list of games!' class='input'>
-        </form>
+                <label for="username" class="label">Type in your username:</label>
+                <input type="text" name="username" id="username" class="input" required maxlength="40" value="<?php echo htmlspecialchars($currentUsername); ?>">
+                <label for="title" class="label">Type in the title of the game:</label>
+                <input type="text" name="title" id="title" class="input" maxlength="80">
+                <label for="description" class="label">Type in a short description of the game:</label>
+                <textarea rows="4" cols="30" name="description" id="description" class="description" maxlength="255"></textarea>
+                <input type="submit" name="add_game" value="Add a game to a list" class="input">
+                <!-- Visar meddelande om en spel har lagts till i listan eller om åtgärd krävs -->
+                <?php if ($message): ?>
+                    <p class="input"><?php echo htmlspecialchars($message); ?></p>
+                <?php endif; ?>
+                <input type='hidden' name='show_list' value='1'>
+                <input type='submit' value='Show your list of games!' class='input'>
+            </form>
+            <!-- Visar en tabell när $showlist sätts till true via en knapptryckning -->
             <?php if ($showList): ?>
-                <table class="flex">
+                <a id="list-start" style="visibility: hidden;"></a>
+                <table class="flex" id="games-table">
                     <caption>My list of games</caption>
+                    <!-- Visar alla spel i en tabell kopplat till en userID -->
                     <?php if (!empty($games)): ?>
+                        <!-- skapar tabellelement för varje spel i databasen som finns i arrayen $games -->
                         <?php foreach ($games as $index => $game): ?>
                         <tr>
                             <th>Order of play:</th>
@@ -60,27 +65,31 @@ require_once 'crud-functions.php';
                         <tr>
                             <th>Game finished?:</th>
                             <td>
-                            <form method="post" style='display:inline;'>
-                                <input type='hidden' name='username' value='<?php echo htmlspecialchars($currentUsername); ?>'>
-                                <input type='hidden' name='game_id' value='<?php echo htmlspecialchars($game["gameID"]); ?>'>
-                                <input type='hidden' name='toggle_completion'>
-                                <input class='buttons toggle-checkbox' type='checkbox' name='is_completed' <?php echo ($game['is_completed']) ? 'checked' : ''; ?>>
-                                <input class='buttons save' type='submit' value='Save'>
-                            </form>
+                                <!-- omredigerar avnädaren till tabellens början när submit knappen trycks -->
+                                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>#list-start">
+                                    <!-- skickar data på ett säkert sätt om vilken användare som gör ändringen -->
+                                    <input type='hidden' name='username' value='<?php echo htmlspecialchars($currentUsername); ?>'>
+                                    <!-- skickar data på ett-Sahert sätt om i vilket spel som ändringen görs -->
+                                    <input type='hidden' name='game_id' value='<?php echo htmlspecialchars($game["gameID"]); ?>'>
+                                    <input type='hidden' name='toggle_completion'>
+                                    <input class='buttons toggle-checkbox' type='checkbox' name='is_completed' <?php echo ($game['is_completed']) ? 'checked' : ''; ?>>
+                                    <input class='buttons save' type='submit' value='Save'>
+                                </form>
                             </td>
                         </tr>
                         <tr>
                             <td colspan="2">
+                                <!-- Visar eller döljer formuläret till uppdatering av spel efter knapptryckning -->
                                 <input type="checkbox" id="toggle-update-form-<?php echo htmlspecialchars($game["gameID"]); ?>" style="display:none;">
                                 <label class="buttons change" for="toggle-update-form-<?php echo htmlspecialchars($game["gameID"]); ?>">Change</label>
                                 
-                                <!-- Update Form -->
+                                <!-- visar formuläret till uppdatering av rätt spel -->
                                 <div class="update-form" id='update-form-<?php echo htmlspecialchars($game["gameID"]); ?>'>
-                                    <form method='post'>
+                                    <form method='post' action="<?php echo $_SERVER['PHP_SELF']; ?>#list-start">
                                         <input type='hidden' name='username' value='<?php echo htmlspecialchars($currentUsername); ?>'>
                                         <input type='hidden' name='game_id' value='<?php echo htmlspecialchars($game["gameID"]); ?>'>
                                         <label for='new_title'>New Title:</label><br>
-                                        <input type='text' name='title' required><br>
+                                        <input type='text' name='title' maxlength='80' required><br>
                                         <label for='new_description'>New Description:</label><br>
                                         <textarea rows="4" cols="30" name="description" class="description" maxlength="255" required></textarea><br>
                                         <input class='buttons update-game' type='submit' name='update_game' value='Update Game'>
@@ -90,7 +99,7 @@ require_once 'crud-functions.php';
                         </tr>
                         <tr class="space-between">
                             <td colspan="2">
-                                <form method="post" style="display: inline;">
+                                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>#list-start">
                                     <input type="hidden" name="username" value="<?php echo htmlspecialchars($currentUsername); ?>">
                                     <input type="hidden" name="game_id" value="<?php echo htmlspecialchars($game['gameID']); ?>">
                                     <input class='buttons delete' type="submit" name="delete_game" value="Delete">
@@ -101,6 +110,7 @@ require_once 'crud-functions.php';
                             <td colspan="2">&nbsp;</td>
                         </tr>
                         <?php endforeach; ?>
+                    <!-- Visar meddelande om inga spel finns i listan -->
                     <?php else: ?>
                         <tr><td colspan="2">Add a new game to your list.</td></tr>
                     <?php endif; ?>
@@ -108,7 +118,7 @@ require_once 'crud-functions.php';
             <?php endif; ?>
         </main>
         <footer id="footer" class="flex">
-            <p>Copyright 2024 &copy; Viktor Ekström</p>
+            <p>Copyright 2025 &copy; Viktor Ekström</p>
         </footer>
     </div>
 </body>
